4e215abfe8f74ea1dcc226f7e986885c69075f6a
nutthapong-n <nutthapong.n@ku.th>
2017-05-26 19:39:57 +0700

    Add project detail page and register option

---
+++ projectDetail.php
@@ -0,0 +1,184 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png" />
+    <link rel="icon" type="image/png" href="assets/img/favicon.png" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+    <title>Project Master 007</title>
+
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+
+    <!--     Fonts and icons     -->
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
+
+    <!-- CSS Files -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="assets/css/material-dashboard.css" rel="stylesheet"/>
+    <link href="documentation/css/demo-documentation.css" rel="stylesheet" />
+
+    <script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js"></script>
+
+    <style>
+
+        body{
+            /*//background-image: url('assets/img/bg4.jpg');*/
+            background-color: lightgray;
+            background-attachment: fixed;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+        #myCard{
+           overflow:auto;
+           width: 90%; 
+           height:500px; 
+           font-size: 14px;
+           margin-left: 5%;
+           margin-right: 5%;
+       }
+       pre.prettyprint{
+        background-color: #eee;
+        border: 0px;
+        margin-bottom: 60px;
+        margin-top: 30px;
+        padding: 20px;
+        text-align: left;
+    }
+    .atv, .str{
+        color: #05AE0E;
+    }
+    .tag, .pln, .kwd{
+        color: #3472F7;
+    }
+    .atn{
+        color: #2C93FF;
+    }
+    .pln{
+        color: #333;
+    }
+    .com{
+        color: #999;
+    }
+    .space-top{
+        margin-top: 50px;
+    }
+    .area-line{
+        border: 1px solid #999;
+        border-left: 0;
+        border-right: 0;
+        color: #666;
+        display: block;
+        margin-top: 20px;
+        padding: 8px 0;
+        text-align: center;
+    }
+    .area-line a{
+        color: #666;
+    }
+    .container-fluid{
+        padding-right: 15px;
+        padding-left: 15px;
+    }
+    .modal-header, h3, .close {
+        background-color: purple;
+        color:white !important;
+        text-align: center;
+        font-size: 30px;
+    }
+
+</style>
+</head>
+<body>
+    <?php 
+    include "component/navbar.php";
+
+    $pID = $_GET["proID"];
+        // echo $path;
+
+
+    ?>
+    <div class="col-md-12" >
+        <div class="card" >
+            <div class="card-header " data-background-color="purple">
+                <h2 class="title">JOB NAME <?php echo $pID; ?></h2>
+            </div>
+            <div class="card-content"  >
+                <h4 class="title">Details</h4>
+
+                <p class="category">Sit adipisicing excepteur aliquip velit sit velit ut laboris dolor sint sit. Lorem ipsum ea labore enim sit cupidatat consectetur culpa sint non tempor esse consequat officia ullamco labore irure. Est qui esse culpa dolor sit voluptate quis ut veniam excepteur cupidatat. Nulla laboris labore ad pariatur ullamco anim et laborum dolore incididunt mollit ullamco. Do dolore cillum in non id quis est sed voluptate adipisicing sint ullamco aute magna. Lorem ipsum magna pariatur qui mollit in anim ad pariatur anim ut enim ut eiusmod dolore in. Lorem ipsum sunt exercitation tempor ut minim dolore mollit qui id consequat eiusmod eiusmod adipisicing id. Enim nostrud excepteur elit dolor ea sit labore duis dolore consectetur ut ut. Lorem ipsum esse qui occaecat et ea sint tempor non aliquip ad quis adipisicing labore sunt consectetur. Ut commodo amet exercitation ullamco minim proident ut nostrud magna nostrud reprehenderit do ea tempor. Commodo dolor dolor aliqua elit irure quis dolor cupidatat. Lorem ipsum ad commodo in consectetur laboris fugiat est aute sint.</p>
+                <br>
+                <p class="category">Qui in id id in mollit dolor dolore in anim ea minim consectetur culpa mollit consequat. Mollit enim ut veniam veniam ut minim sunt reprehenderit adipisicing cillum deserunt qui. Enim in veniam aute elit et dolor voluptate eu anim cupidatat et sit sint. Lorem ipsum dolor irure id in fugiat amet laborum non pariatur tempor consectetur. Ex ullamco in do sunt eu cupidatat velit in velit enim laborum officia sit in ullamco fugiat fugiat. Lorem ipsum excepteur tempor ullamco laborum commodo amet duis non mollit dolor in enim. Nostrud elit quis ullamco pariatur quis amet excepteur laborum tempor ullamco anim ullamco ex. Culpa cupidatat labore ex mollit in aliqua sit tempor mollit. Aute ut occaecat excepteur excepteur culpa in eu cupidatat dolor voluptate deserunt culpa cupidatat in est adipisicing magna. Nulla ex non amet labore do consectetur sit eiusmod velit cupidatat sint occaecat ad laboris dolor dolore cupidatat ut. Lorem ipsum ut qui nulla dolor nisi fugiat incididunt eu adipisicing do.</p>
+                <br>
+                <p class="category">Consectetur in ut ex voluptate ullamco irure incididunt nulla proident. Lorem ipsum duis dolor excepteur ut in mollit non ullamco consectetur consectetur fugiat non voluptate magna culpa minim id dolor. Velit officia commodo esse ut excepteur duis ullamco pariatur enim reprehenderit veniam exercitation laboris in excepteur velit excepteur. Amet commodo et aliqua fugiat officia anim tempor officia laboris. Deserunt amet id reprehenderit id tempor labore fugiat elit pariatur sed aliquip. Id pariatur dolor in mollit officia reprehenderit do amet dolor dolore ut. Ut ut adipisicing incididunt eiusmod ut duis ut labore mollit non. Amet in fugiat officia ullamco dolore quis commodo culpa velit consequat nisi. Aliquip officia incididunt aliqua consequat enim elit magna sed laboris dolore aute laboris culpa excepteur culpa eiusmod exercitation aliqua. Adipisicing dolor irure duis commodo aliqua ut sunt fugiat deserunt eu laborum nostrud. Exercitation voluptate reprehenderit consectetur est magna fugiat ut minim velit tempor ut proident irure id. Culpa et eu culpa nisi ullamco qui do duis dolore amet cupidatat ad dolor cupidatat. Lorem ipsum esse occaecat anim irure veniam magna exercitation sunt elit deserunt. Elit qui laborum ea duis in aute in nisi veniam sint ut aliquip nisi eiusmod enim voluptate. Laboris adipisicing ea minim pariatur incididunt anim ut et et cupidatat ut amet cillum veniam. Lorem ipsum est duis elit exercitation excepteur cillum mollit nulla ad aute est labore. Tempor do labore in in dolore ut amet in dolore fugiat in exercitation laborum. Commodo cillum eiusmod occaecat ex ut est enim eu laboris proident. Est pariatur laborum consectetur deserunt duis eiusmod dolor dolor culpa ut. Ut minim velit nulla aliquip proident ad occaecat ea est in exercitation laboris consectetur veniam nisi officia tempor.</p>
+                <br>
+                <p class="category">Laboris amet et in amet excepteur commodo deserunt id do eiusmod. Lorem ipsum deserunt in quis dolor officia deserunt sit consequat enim aute culpa nisi in adipisicing duis minim amet. Do nostrud ut minim et irure qui labore velit fugiat. Lorem ipsum non aliquip ad velit elit enim consequat culpa irure aute ea non minim ut ullamco. Aute laborum dolor dolor officia eiusmod velit sint proident commodo consequat aute esse dolore in cupidatat exercitation. Lorem ipsum est amet anim aute adipisicing reprehenderit dolor in laborum nulla ad proident proident elit. Est elit mollit qui dolore ut occaecat pariatur enim sint culpa incididunt mollit aliquip amet ullamco nostrud. Irure in aliqua esse eu velit aliqua exercitation aliquip qui deserunt mollit amet velit est id. Sit excepteur irure aliquip dolor ut elit quis nulla irure officia esse mollit velit ut excepteur exercitation. Est commodo consequat sint exercitation exercitation duis eu ut consequat consectetur dolor nulla nostrud ex velit ad dolore. Minim velit reprehenderit ea tempor laboris ullamco sint dolor amet. Lorem ipsum elit excepteur aliquip laborum in occaecat mollit duis velit fugiat laborum reprehenderit deserunt tempor velit mollit veniam ad. Laborum consequat proident laboris in ea ullamco elit veniam tempor dolor ut eu. Lorem ipsum consequat qui ad ut exercitation sed in velit eu mollit incididunt nostrud voluptate cillum. Mollit tempor commodo laboris dolore ad mollit adipisicing duis cupidatat sit cillum. Dolor eiusmod tempor commodo officia qui cupidatat officia ut incididunt in pariatur labore labore ut. In proident ut sed cillum do aute commodo commodo cupidatat mollit id consequat eiusmod laborum sed duis. Do laboris est excepteur sunt fugiat minim id exercitation incididunt voluptate voluptate culpa elit anim tempor adipisicing ullamco. Amet tempor cillum proident laboris commodo enim mollit do amet consequat aute dolor id officia minim tempor elit do. In eu in duis adipisicing non exercitation exercitation reprehenderit mollit nostrud velit cupidatat tempor labore.</p>
+                <br>
+
+                <h4 class="title">Requirements</h4>
+                <p class="category">Sit adipisicing excepteur aliquip velit sit velit ut laboris dolor sint sit. Lorem ipsum ea labore enim sit cupidatat consectetur culpa sint non tempor esse consequat officia ullamco labore irure. Est qui esse culpa dolor sit voluptate quis ut veniam excepteur cupidatat. Nulla laboris labore ad pariatur ullamco anim et laborum dolore incididunt mollit ullamco. Do dolore cillum in non id quis est sed voluptate adipisicing sint ullamco aute magna. Lorem ipsum magna pariatur qui mollit in anim ad pariatur anim ut enim ut eiusmod dolore in. Lorem ipsum sunt exercitation tempor ut minim dolore mollit qui id consequat eiusmod eiusmod adipisicing id. Enim nostrud excepteur elit dolor ea sit labore duis dolore consectetur ut ut. Lorem ipsum esse qui occaecat et ea sint tempor non aliquip ad quis adipisicing labore sunt consectetur. Ut commodo amet exercitation ullamco minim proident ut nostrud magna nostrud reprehenderit do ea tempor. Commodo dolor dolor aliqua elit irure quis dolor cupidatat. Lorem ipsum ad commodo in consectetur laboris fugiat est aute sint.</p>
+                <br>
+
+                <button type="button" class="btn btn-primary" data-toggle="collapse" data-target="#positions">Register to this job</button>
+
+                <div id="positions" class="collapse">
+                    <h4 class="title">Our positions</h4>
+                    <button type="button" class="btn btn-primary active">Frot-End Programmer (Javascript, HTML, CSS)</button>
+                    <button type="button" class="btn btn-primary disabled">Back-End Programmer (PHP, blah blah blah)</button>
+                    <button type="button" class="btn btn-primary active">Frot-End Programmer</button>
+                    <button type="button" class="btn btn-primary disabled">Back-End Programmer (PHP, blah blah blah)</button>
+                    <button type="button" class="btn btn-primary active">Frot-End Programmer</button>
+                    <button type="button" class="btn btn-primary disabled">Back-End Programmer (PHP, blah blah blah)</button>
+                    <button type="button" class="btn btn-primary active">Frot-End Programmer</button>
+                    <button type="button" class="btn btn-primary disabled">Back-End Programmer</button>
+                    <button type="button" class="btn btn-primary active">Frot-End Programmer (Javascript, HTML, CSS)</button>
+                    <button type="button" class="btn btn-primary disabled">Back-End Programmer (PHP, blah blah blah)</button>
+                </div>
+                
+            </div>
+            <div class="card-footer">
+                <div class="stats">
+                    <i class="material-icons">access_time</i> updated 4 minutes ago
+                    <p class="category"><span class="text-success"><i class="fa fa-long-arrow-up"></i> 55%  </span> increase in today sales.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+<!--   Core JS Files   -->
+<script src="assets/js/jquery-3.1.0.min.js" type="text/javascript"></script>
+<script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="assets/js/material.min.js" type="text/javascript"></script>
+
+<!--  Charts Plugin -->
+<script src="assets/js/chartist.min.js"></script>
+
+<!--  Notifications Plugin    -->
+<script src="assets/js/bootstrap-notify.js"></script>
+
+<!--  Google Maps Plugin    -->
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script>
+
+<!-- Material Dashboard javascript methods -->
+<script src="assets/js/material-dashboard.js"></script>
+
+<!-- Material Dashboard DEMO methods, don't include it in your project! -->
+<script src="assets/js/demo.js"></script>
+
+<script>
+    var header_height;
+    var fixed_section;
+    var floating = false;
+
+    $().ready(function(){
+        suggestions_distance = $("#suggestions").offset();
+        pay_height = $('.fixed-section').outerHeight();
+        $('.modal').appendTo("body");
+        $(window).on('scroll', md.checkScrollForTransparentNavbar);
+        demo.initDocumentationCharts();
+    });
+</script>
+</html>
