3dc599281a67a34e53d39a1a566c0c83d6edd418
Watcharagorn mayomthong <first927@live.com>
2017-05-30 00:49:34 +0700

    fixed profile modal + addButton-file

---
+++ component/profileModalButton.php
@@ -0,0 +1,18 @@
+<button class="btn btn-warning btn-fab btn-fab-mini btn-round" id="profile-edit-btn" data-toggle="tooltip" data-placement="top" title="edit"  >
+    <i class="material-icons edit-icon">build</i>
+</button>
+<button class="btn btn-success btn-fab btn-fab-mini btn-round" id="profile-save-btn" data-toggle="tooltip" data-placement="top" title="save"  >
+    <i class="material-icons edit-icon">save</i>
+</button>
+<button class="btn btn-info btn-fab btn-fab-mini btn-round" id="profile-port-btn" data-toggle="tooltip" data-placement="top" title="portfolio"  >
+    <i class="material-icons edit-icon">folder</i>
+</button>
+<button class="btn btn-danger btn-fab btn-fab-mini btn-round" id="profile-backport-btn" data-toggle="tooltip" data-placement="top" title="portfolio"  >
+    <i class="material-icons edit-icon">backspace</i>
+</button>
+<button class="btn btn-success btn-fab btn-fab-mini btn-round" id="invite-btn" data-toggle="tooltip" data-placement="top" title="invite" >
+        <i class="material-icons main-icon">add</i>
+</button>
+<button class="btn btn-danger btn-fab btn-fab-mini btn-round" id="back-btn" data-toggle="tooltip" data-placement="top" title="back"  >
+        <i class="material-icons main-icon">backspace</i>
+</button>
--- component/userprofileModal.inc.php
+++ component/userprofileModal.inc.php
@@ -6,26 +6,8 @@
             
             <div class="card card-profile">
                 <div id="profile-btn">
-                    <button class="btn btn-warning btn-fab btn-fab-mini btn-round" id="profile-edit-btn" data-toggle="tooltip" data-placement="top" title="edit"  >
-	                    <i class="material-icons edit-icon">build</i>
-                    </button>
-                    <button class="btn btn-success btn-fab btn-fab-mini btn-round" id="profile-save-btn" data-toggle="tooltip" data-placement="top" title="save"  >
-	                    <i class="material-icons edit-icon">save</i>
-                    </button>
-                    <button class="btn btn-info btn-fab btn-fab-mini btn-round" id="profile-port-btn" data-toggle="tooltip" data-placement="top" title="portfolio"  >
-	                    <i class="material-icons edit-icon">folder</i>
-                    </button>
-                    <button class="btn btn-danger btn-fab btn-fab-mini btn-round" id="profile-backport-btn" data-toggle="tooltip" data-placement="top" title="portfolio"  >
-	                    <i class="material-icons edit-icon">backspace</i>
-                    </button>
-                    <button class="btn btn-success btn-fab btn-fab-mini btn-round" id="invite-btn" data-toggle="tooltip" data-placement="top" title="invite" >
-                            <i class="material-icons main-icon">add</i>
-                    </button>
-                    <button class="btn btn-danger btn-fab btn-fab-mini btn-round" id="back-btn" data-toggle="tooltip" data-placement="top" title="back"  >
-                            <i class="material-icons main-icon">backspace</i>
-                    </button>
+                    <?php include "profileModalButton.php"; ?>
                 </div>
-                
                 
                 
     			<div class="card-avatar" >
@@ -36,7 +18,7 @@
     			</div>
     			<div class="content">
                     <div id="showForm">
-                        <div class="col-md-6" id="profile-detail" style="margin-left:25% ;">
+                        <div  id="profile-detail" >
                             <?php include "userInformation.inc.php"; ?>
                         </div>
                     </div>
